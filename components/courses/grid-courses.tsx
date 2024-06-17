a67f31b3f1d5f8ff29598ecb67eadc9b72d14dad
ecampusano <ecampusano@dgcp.gob.do>
2024-06-17 19:09:15 -0400

    back commit

--- components/courses/grid-courses.tsx
+++ components/courses/grid-courses.tsx
@@ -11,31 +11,36 @@ interface GridCoursesProps {
 }
 
 export async function GridCourses({ query }: GridCoursesProps) {
-    const courses: CourseDB[] = await prisma.course.findMany({
+    const courses: CourseDB = await prisma.course.findMany({
         where: {
             course_title: {
                 contains: query,
             },
-            course_description: {
-                contains: query,
-            },
-            facilitator: {
-                facilitator_name: {
-                    contains: query,
-                },
-            },
+        //     course_description: {
+        //         contains: query,
+        //     },
+        //     facilitator: {
+        //         facilitator_name: {
+        //             contains: query,
+        //         },
+        //     },
         },
         include: {
-            course_flayer: true,
-            facilitator: {
-                include: {
-                    facilitator_socials: true,
-                    facilitator_image: true,
-                },
-            },
-            meeting: true,
+            Facilitator: true,
+            Meeting: true,
+            Flayer: true,
+            // course_flayer: true,
+            // facilitator: {
+            //     include: {
+            //         facilitator_socials: true,
+            //         facilitator_image: true,
+            //     },
+            // },
+            // meeting: true,
         },
     });
+
+    console.log({ courses });
 
     return (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
--- types/index.d.ts
+++ types/index.d.ts
@@ -15,13 +15,13 @@ interface CourseDB {
   id: string;
   course_title: string;
   course_description: string;
-  course_flayer: FlayerDB;
+  Flayer: FlayerDB;
   facilitatorId: string;
   meetingId: string;
   createdAt: Date;
   updatedAt: Date;
-  facilitator: FacilitatorDB;
-  meeting: MeetingDB;
+  Facilitator: FacilitatorDB;
+  Meeting: MeetingDB;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -54,46 +54,33 @@ interface FlayerDB {
 
 
 
-// // course response from api
-// interface CoursePrismaResponse {
-//   id: number;
+
+// interface Course<T> {
 //   course_title: string;
 //   course_description: string;
-//   course_flayer: string;
-//   facilitatorId: number;
-//   meetingId: number;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   facilitator: Facilitator;
+//   course_flayer: File | null | string;
+//   facilitator: T; // Facilitator | string;
 //   meeting: Meeting;
 // }
 
-interface Course<T> {
-  course_title: string;
-  course_description: string;
-  course_flayer: File | null | string;
-  facilitator: T; // Facilitator | string;
-  meeting: Meeting;
-}
+// interface Social {
+//   instagram: string;
+//   facebook: string;
+//   linkedin: string;
+//   mail: string;
+// }
 
-interface Social {
-  instagram: string;
-  facebook: string;
-  linkedin: string;
-  mail: string;
-}
+// interface Facilitator {
+//   facilitator_name: string;
+//   facilitator_role: string;
+//   facilitator_skills: string[];
+//   facilitator_description: string;
+//   facilitator_image: File | null | string;
+//   facilitator_socials: Social;
+// }
 
-interface Facilitator {
-  facilitator_name: string;
-  facilitator_role: string;
-  facilitator_skills: string[];
-  facilitator_description: string;
-  facilitator_image: File | null | string;
-  facilitator_socials: Social;
-}
-
-interface Meeting {
-  url: string;
-  datetime: string;
-  details: string;
-}
+// interface Meeting {
+//   url: string;
+//   datetime: string;
+//   details: string;
+// }
